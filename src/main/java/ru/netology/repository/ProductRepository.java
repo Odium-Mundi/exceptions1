1a3c6671b9d5d16bbc74d47d146acdf53b6906c7
Odium-Mundi <violetta_wolf@mail.ru>
2020-05-29 12:29:09 +0300

    add exception

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package ru.netology.repository;
 
 import ru.netology.domain.Product;
+import ru.netology.exceptions.NotFoundException;
 
 public class ProductRepository {
   private Product[] items = new Product[0];
@@ -27,7 +28,10 @@ public class ProductRepository {
     return null;
   }
 
-  public void removeById(int id) {
+  public void removeById(int id) throws NotFoundException {
+    if (findById(id) == null) {
+      throw new NotFoundException("Element with id: " + id + " not found!");
+    }
     int length = items.length - 1;
     Product[] tmp = new Product[length];
     int index = 0;
--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -3,19 +3,42 @@ package ru.netology.repository;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
+import ru.netology.exceptions.NotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
-  private ProductRepository repository = new ProductRepository();
-  private Book coreJava = new Book();
+    private ProductRepository repository = new ProductRepository();
+    private Book coreJava = new Book();
+    private Product book1 = new Book(1, "book1", 20, "i", 20, 2020);
+    private Product book2 = new Book(2, "book1", 20, "i", 20, 2020);
 
-  @Test
-  public void shouldSaveOneItem() {
-    repository.save(coreJava);
+    @Test
+    public void shouldSaveOneItem() {
+        repository.save(coreJava);
 
-    Product[] expected = new Product[]{coreJava};
-    Product[] actual = repository.findAll();
-    assertArrayEquals(expected, actual);
-  }
+        Product[] expected = new Product[]{coreJava};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveByIdIfExists() {
+        repository.save(book1);
+        repository.save(book2);
+
+        repository.removeById(1);
+        Product[] expected = new Product[]{book2};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveByIdIfNoExists() {
+        repository.save(book1);
+        repository.save(book2);
+
+        assertThrows(NotFoundException.class, () -> repository.removeById(3));
+    }
+
 }
